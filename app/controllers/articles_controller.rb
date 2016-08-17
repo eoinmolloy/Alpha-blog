e8b0d1087d31d658ca13ff4ea81491f106697a32
Rails Course <eoinmolloy@outlook.com>
2016-08-17 17:36:45 +0000

    Ability to destroy articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -26,6 +26,14 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
+  
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
